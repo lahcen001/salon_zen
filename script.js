98d17143d3a3675418357507344bd9469b01aa6d
lahcen001e <lahcen0000lh@gmail.com>
2021-09-04 01:03:53 +0100

    scroll_up

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ $(document).ready(function () {
         midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
     });
 });
+
+var mybutton = document.getElementById("gotoup");
+
+
+window.onscroll = function () { scrollFunction() };
+function scrollFunction() {
+    if (document.body.scrollTop > 15 || document.documentElement.scrollTop > 15) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
