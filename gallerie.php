33b4a0129012735d3a4dc159212e8d94b04b2206
lahcen001e <lahcen0000lh@gmail.com>
2022-02-20 23:24:41 +0100

    to php

--- gallerie.php
+++ gallerie.php
@@ -94,56 +94,9 @@
         </div>
     </div>
 
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary ">
-        <a class="navbar-brand " href="https://webvok.net/demo/salonzen/">
-            <img src="./img/logo.png" alt="" class="logo">
-        </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mx-auto">
-                <li class="nav-item">
-                    <a class="nav-link active Luxia_2" href="https://webvok.net/demo/salonzen/">Acceil
-                        <span class="visually-hidden">(current)</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link Luxia_2" href="#">Services</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link Luxia_2" href="offre.html">Offres</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link Luxia_2 " href="gallerie.html">Galerie</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link Luxia_2" href="#">Equipe</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link Luxia_2" href="a_propos.html">A propos</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link Luxia_2" href="#">Contact</a>
-                </li>
-
-
-                <li class="nav-item">
-                    <a class="nav-link Luxia_2" id="myBtn" href="#"> <i class="fa fa-globe" aria-hidden="true"></i>
-                        FR</a>
-                </li>
-
-
-
-
-            </ul>
-
-
-
-        </div>
-    </nav>
+    <?php
+	include 'navbar.php';
+?>
 
     <div class="container">
         <div class="row">
